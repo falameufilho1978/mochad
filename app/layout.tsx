8233099cd90c5a3da0eb62d5d8e8fdefbb7793a9
jkbwrs <120724051+jkbwrs@users.noreply.github.com>
2024-04-05 14:01:55 +0200

    favicon + webclip

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link href="../mo-webclip.png" rel="apple-touch-icon"></link>
+        <link href="../mo-favicon.png" rel="shortcut icon" type="image/x-icon"></link>
+      </head>
       <body>{children}</body>
     </html>
   );
